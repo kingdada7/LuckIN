51bfcd398c1b5049da798db6aa652a9d39ef5fab
kingdada7 <toluwadada7@gmail.com>
2025-09-12 12:27:33 +0100

    hosted on render

--- frontend/src/utils/apiPaths.js
+++ frontend/src/utils/apiPaths.js
@@ -1,37 +1,39 @@
-export const API_BASE_URL = "http://localhost:5000";
+export const API_BASE_URL =
+  process.NODE_ENV === "production"
+    ? "https://luckin-vubx.onrender.com"
+    : "http://localhost:5000";
 
 export const API_ENDPOINTS = {
-    AUTH: {
-        REGISTER: "/api/auth/register",
-        LOGIN: "/api/auth/login",
-        GET_PROFILE: "/api/auth/profile",
-    },
-    
-    USER: {
-        GET_USERS: "/api/users",
-        GET_USER_BY_ID: (userId) => `/api/users/${userId}`,
-        CREATE_USER: "/api/users",
-        UPDATE_USER: (userId) => `/api/users/${userId}`,
-        DELETE_USER: (userId) => `/api/users/${userId}`
-    },
+  AUTH: {
+    REGISTER: "/api/auth/register",
+    LOGIN: "/api/auth/login",
+    GET_PROFILE: "/api/auth/profile",
+  },
 
-    TASKS: {
-        GET_DASHBOARD_DATA: "/api/tasks/dashboard-data",
-        GET_USER_DASHBOARD_DATA: "/api/tasks/user-dashboard-data",
-        GET_ALL_TASKS: "/api/tasks",
-        GET_TASK_BY_ID: (taskId) => `/api/tasks/${taskId}`,
-        CREATE_TASK: "/api/tasks",
-        UPDATE_TASK: (taskId) => `/api/tasks/${taskId}`,
-        DELETE_TASK: (taskId) => `/api/tasks/${taskId}`
-    },
+  USER: {
+    GET_USERS: "/api/users",
+    GET_USER_BY_ID: (userId) => `/api/users/${userId}`,
+    CREATE_USER: "/api/users",
+    UPDATE_USER: (userId) => `/api/users/${userId}`,
+    DELETE_USER: (userId) => `/api/users/${userId}`,
+  },
 
-    REPORTS: {
-        EXPORT_TASKS: "/api/reports/export/tasks",
-        EXPORT_USERS: "/api/reports/export/users"
-    },
+  TASKS: {
+    GET_DASHBOARD_DATA: "/api/tasks/dashboard-data",
+    GET_USER_DASHBOARD_DATA: "/api/tasks/user-dashboard-data",
+    GET_ALL_TASKS: "/api/tasks",
+    GET_TASK_BY_ID: (taskId) => `/api/tasks/${taskId}`,
+    CREATE_TASK: "/api/tasks",
+    UPDATE_TASK: (taskId) => `/api/tasks/${taskId}`,
+    DELETE_TASK: (taskId) => `/api/tasks/${taskId}`,
+  },
 
-    IMAGE: {
-        UPLOAD: "/api/auth/upload-profile-image"
-    }
+  REPORTS: {
+    EXPORT_TASKS: "/api/reports/export/tasks",
+    EXPORT_USERS: "/api/reports/export/users",
+  },
+
+  IMAGE: {
+    UPLOAD: "/api/auth/upload-profile-image",
+  },
 };
-  
